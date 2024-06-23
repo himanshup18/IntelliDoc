2b09182b36e157a2d9c39029b33cf9ab1e8d5262
himanshup18 <138312336+himanshup18@users.noreply.github.com>
2024-06-23 18:34:09 +0530

    Add files via upload

---
+++ Client/src/assests/assets.js
@@ -0,0 +1,44 @@
+import history_icon from './history_icon.png'
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus.png'
+import question_icon from './question_icon.png'
+import setting_icon from './setting_icon.png'
+import bulb_icon from './bulb_icon.png'
+import compass_icon from './compass_icon.png'
+import gallery_icon from './gallery_icon.png'
+import mic_icon from './mic_icon.png'
+import user_icon from './user_icon.png'
+import youtube_icon from './youtube_icon.png'
+import message_icon from './message_icon.png'
+import code_icon from './code_icon.png'
+import send_icon from './send_icon.png'
+import gemini_icon from './gemini_icon.png'
+import stethoscope from './stethoscope.png'
+import doctor_icon from './doctor.png'
+import t247_icon from './24-hours.png'
+import symptom from './symptom.png'
+import healthcare from './healthcare.png'
+
+export const assets = {
+    history_icon,
+    menu_icon,
+    plus_icon,
+    question_icon,
+    setting_icon,
+    bulb_icon,
+    compass_icon,
+    gallery_icon,
+    mic_icon,
+    user_icon,
+    youtube_icon,
+    message_icon,
+    code_icon,
+    send_icon,
+    gemini_icon,
+    stethoscope,
+    doctor_icon,
+    t247_icon,
+    healthcare,
+    symptom
+
+}
